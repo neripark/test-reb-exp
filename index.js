94f687f6ad5ed3f1b3f14e722dcb532a3a031268
neripark <neripark@gmail.com>
2020-08-25 16:24:20 +0900

    test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // const reg = new RegExp("^https?:\/\/[^ "]+$/");
 // const reg = new RegExp("^https?://.*\\..*");
 // const reg = new RegExp("^https?://[^/? #]*\\..*");
-const reg = new RegExp("^https?://[a-zA-Z0-9\-]*\\..*");
+// const reg = new RegExp("^https?://[a-zA-Z0-9\-]*\\..*");
+const reg = new RegExp("^https?:\/\/.*");
 
 // memo
 // [^\x01-\x7E] 全角文字
@@ -12,6 +13,7 @@ console.log("--- true ---")
 console.log(reg.test("https://google.com")); // true
 console.log(reg.test("https://google.jp/hogehoge/jovak?aaa=123")); // true
 console.log(reg.test("http://google.com")); // true
+console.log(reg.test("https://www.youtube.com/watch?v=2VQmwqWvLUw")); // true
 
 console.log("--- false ---")
 console.log(reg.test("https//google.com"));
@@ -20,4 +22,15 @@ console.log(reg.test("https:/google.com"));
 console.log(reg.test("aaaa https://google.com"));
 console.log(reg.test("https://google/hogehoge.com"));
 console.log(reg.test("https://google?hogehoge.com"));
+
+
+
+//
+let url = "<meta charset='utf-8'><a href=\"https://www.youtube.com/watch?v=xxxxxxxxxxxx\">https://www.youtube.com/watch?v=xxxxxxxxxxxx</a>";
+const reg = /<.+?>/g;
+
+
+console.log(reg.test(url));
+console.log("trimmed", url.replace(reg, ""));
+console.log("urlかどうか", /https?:\/\/.+/.test(url))
 
